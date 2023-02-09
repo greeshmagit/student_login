7b66c0a6bf3b332823ea874fb01904b7638f7ade
greeshmagit <106822996+greeshmagit@users.noreply.github.com>
2023-02-09 14:56:09 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,14 @@
+import subprocess
+
+# Source and destination file paths
+src = "nginx:/usr/share/nginx/html/*"
+dst = "ec2_user@13.127.107.231:/opt/build"
+
+# Copy the file using SCP
+subprocess.run(["scp", src, dst])
+
+'''
 from subprocess import subprocess
 p = subprocess.Popen(["scp", "index.html", "ec2@13.127.107.231:/opt/build"])
 sts = os.waitpid(p.pid, 0)
+'''
